42e3aab0c06f17d44a2be9aea0e8a2deafc39d60
Eliott Teissonniere <git.eliott@teissonniere.org>
2023-01-27 11:20:11 -0800

    init app

---
+++ cmd/root.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2023 Eliott Teissonniere
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "app-store-connect-cli",
+	Short: "A simple CLI tool to interact with the App Store Connect API",
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.app-store-connect-cli.yaml)")
+}
